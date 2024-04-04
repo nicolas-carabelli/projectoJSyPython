c8d4af2a54f06b0266f7d25cfecb00b266e3290e
nicolas-carabelli <fnicolas.carabelli@gmail.com>
2024-04-03 23:05:20 -0300

    Documentando el código
    Se documenta según normas estándares.

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -1,13 +1,15 @@
-// Establece la fecha de lanzamiento del álbum
-const releaseDate = new Date('April 19, 2024 00:00:00').getTime();
+/**
+ * Establece la fecha de lanzamiento del álbum y actualiza la cuenta regresiva cada segundo.
+ * Muestra el tiempo restante en días, horas, minutos y segundos en los elementos HTML correspondientes.
+ * Cuando la cuenta regresiva termina, muestra un mensaje indicando que el álbum ha sido lanzado.
+ */
+const releaseDate = new Date('April 19, 2024 00:00:00').getTime(); // Fecha de lanzamiento del álbum
 
-// Actualiza la cuenta regresiva cada segundo
-const interval = setInterval(function() {
-    // Obtiene la fecha y hora actual
-    const now = new Date().getTime();
 
-    // Encuentra la distancia entre ahora y la fecha de lanzamiento
-    const distance = releaseDate - now;
+const interval = setInterval(function() { // Inicia un intervalo que se ejecuta cada segundo
+    const now = new Date().getTime(); // Obtiene la fecha y hora actual
+
+    const distance = releaseDate - now;  // Calcula la distancia entre la fecha actual y la de lanzamiento
 
     // Cálculos de tiempo para días, horas, minutos y segundos
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -15,7 +17,7 @@ const interval = setInterval(function() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Muestra el resultado en los elementos con id="days", "hours", "minutes" y "seconds"
+    // Actualiza los elementos HTML con los valores calculados
     document.getElementById('days').innerText = days;
     document.getElementById('hours').innerText = hours;
     document.getElementById('minutes').innerText = minutes;
